ef0eb9c2dde8df3031ac137de806d93e0bf89599
elcrack56 <darwinsteven3@gmail.com>
2024-10-22 22:38:10 -0500

    Agregar la cabecera en el layuot

--- ManejoDeEquiposLigaPro2024/Models/Equipo.cs
+++ ManejoDeEquiposLigaPro2024/Models/Equipo.cs
@@ -4,10 +4,13 @@ namespace ManejoDeEquiposLigaPro2024.Models
 {
     public class Equipo
     {
-       
+        [Key]
         public int Id { get; set; }
+        [MaxLength(50)]
         public string Nombre { get; set; }
+        [MaxLength(50)]
         public string Ciudad {  get; set; }
+        [MaxLength(20)]
         public int Titulos { get; set; }
         public bool AceptaExtranjeros {  get; set; }
         
--- ManejoDeEquiposLigaPro2024/Models/Estadio.cs
+++ ManejoDeEquiposLigaPro2024/Models/Estadio.cs
@@ -5,9 +5,12 @@ namespace ManejoDeEquiposLigaPro2024.Models
     public class Estadio
     {
         [Key]
-        public int Id { get; set; }  
+        public int Id { get; set; }
+        [MaxLength(100)]
         public string Direccion { get; set; }
+        [MaxLength(50)]
         public string Ciudad { get; set; }
+        [MaxLength(50)]
         public int Capacidad { get; set; }
     }
 }
